fb7885af03e5a49607b6fb8a682de47bc2c72f6b
RafaelN <rafaeln_nascimento@hotmail.com>
2025-05-23 17:21:52 -0300

    alterado pra usar lpush

--- desafios/tasks.py
+++ desafios/tasks.py
@@ -12,32 +12,35 @@ def run_consumer_processar_disponibilidade():
     print("Iniciando consumo...")
     while True:
         method_frame, properties, body = channel.basic_get(queue='service_monitor', auto_ack=False)
-
         if method_frame:
             processar_disponibilidade(channel, method_frame, properties, body)
         else:
             print("Fila vazia. Encerrando consumidor.")
             break
-    
-    
+
 def processar_disponibilidade(ch, method, properties, body):
     try:
-        json_data = json.loads(body)        
+        json_data = json.loads(body)
         timestamp = int(time.time())
         status_code = json_data.get("status_code")
         latency_ms = json_data.get("latency_ms")
         url = json_data.get("url")
-        
-        redis_client.zadd("latencies", {url: latency_ms})
-        # Se for erro (status >= 400), registra no Sorted Set de erros com o timestamp   
+
+        latencies_key = f"latencies:{url}"
+        redis_client.lpush(latencies_key, latency_ms)
+        redis_client.ltrim(latencies_key, 0, 9)
+
+        # Se for erro (status >= 400), registra a url e a hora
         if status_code >= 400:
             redis_client.zadd("errors", {url: timestamp})
             print(f"URL: {url}, Status code: {status_code}, json = {json_data}")
         else:
             print(f"URL: {url}, Status code: {status_code}, json = {json_data}")
-        channel.basic_ack(delivery_tag=method.delivery_tag)
-        
+
+        ch.basic_ack(delivery_tag=method.delivery_tag)
+
     except Exception as e:
         print(f"Erro ao processar mensagem: {e}")
-        channel.basic_nack(delivery_tag=method.delivery_tag, requeue=False)  # descarta
+        ch.basic_nack(delivery_tag=method.delivery_tag, requeue=False)  # descarta
+
 
